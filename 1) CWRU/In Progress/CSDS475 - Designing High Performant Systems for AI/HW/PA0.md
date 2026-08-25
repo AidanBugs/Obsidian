9ed2e115d40e73ec1bda96ad4aeeecb82370d2f5
Extr1n <aidan.bugayong@gmail.com>
2026-08-25 13:50:38 -0400

    CSDS475 HW

---
+++ 1) CWRU/In Progress/CSDS475 - Designing High Performant Systems for AI/HW/PA0.md
@@ -0,0 +1,37 @@
+---
+format: pdf
+---
+
+# 1
+See zip
+
+> Vector1 has a length of 100,000
+
+# 2
+See zip
+
+> Vector2 has a length of 100,000
+
+# 3
+See zip
+
+> Vec1 first 10: -7.36924, -0.826997, -5.62082, 3.57729, 8.69386, 0.388327, -9.30856, 0.594004, -9.84604, -8.6
+6316
+
+> Vec2 first 10: -3.27266, -9.31481, 6.54747, -8.61734, 0.650344, 5.80275, 9.91973, 2.77242, 8.26379, -3.78691
+
+# 4
+The difference between vectors and arrays in C++ is that vectors are resizable while arrays are not.
+
+# 5
+See zip
+
+> Written tto data/hadamard.txt
+
+# 6
+See zip
+
+> Same with a tolerance of 0.0001 which I attributed to rounding errors
+
+# 7
+I had very basic exposure to C++ like 7 or 8 years ago and have not used it since, so I have very little C++ experience. The exercises helped me remember coding in languages like C (which has also been around 3 years now) as I have almost exclusively coded in python for the past few years.
---
+++ 1) CWRU/In Progress/CSDS475 - Designing High Performant Systems for AI/HW/PA0/main.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+double* readFileToArray(const string& filename, size_t& count) {
+    ifstream file1(filename);
+
+    count = 0;
+    double temp;
+    while (file1 >> temp) {
+        ++count;
+    }
+    file1.close();
+
+    double* arr = new double[count];
+
+    ifstream file2(filename);
+
+    for (size_t i = 0; i < count; ++i) {
+        file2 >> arr[i];
+    }
+    file2.close();
+
+    return arr;
+}
+
+
+int main()
+{
+    size_t count1 = 0;
+    double* arr1 = readFileToArray("data/random_vector_1.txt", count1);
+
+    cout << "Problem 1) Vec1 has length ";
+    cout << count1;
+
+    size_t count2 = 0;
+    double* arr2 = readFileToArray("data/random_vector_2.txt", count2);
+
+    cout << "\n\nProblem 2) Vec2 has length ";
+    cout << count2;
+
+  ifstream file("data/random_vector_1.txt");
+  
+  vector<double> vec1;
+
+  copy(
+      istream_iterator<double>(file),
+      istream_iterator<double>(),
+      back_inserter(vec1));
+
+  ifstream file2("data/random_vector_2.txt");
+  
+  vector<double> vec2;
+
+  copy(
+      istream_iterator<double>(file2),
+      istream_iterator<double>(),
+      back_inserter(vec2));
+
+  cout << "\n\nProblem 3) \n\tVec1 first 10: ";
+
+  for (int i = 0; i < 10; i++){
+    cout << vec1[i];
+    cout << ", ";
+  }
+
+  cout << "\n\tVec2 first 10: ";
+
+  for (int i = 0; i < 10; i++){
+    cout << vec2[i];
+    cout << ", ";
+  }
+
+  vector<double> hadamard;
+
+  for (int i = 0; i < vec1.size(); i++){
+    hadamard.push_back(vec1[i]*vec2[i]);
+  }
+
+  ofstream out("data/hadamard.txt");
+
+  copy(hadamard.begin(), hadamard.end(), ostream_iterator<double>(out, " ")); 
+
+  cout << "\n\nProblem 5: Hadamard product written to data/hadamard.txt";
+
+  ifstream file3("data/resulting_vector.txt");
+
+  vector<double> res;
+
+  copy(
+      istream_iterator<double>(file3),
+      istream_iterator<double>(),
+      back_inserter(res));
+
+  hadamard.clear();
+
+  ifstream file4("data/hadamard.txt");
+
+  copy(
+      istream_iterator<double>(file4),
+      istream_iterator<double>(),
+      back_inserter(hadamard));
+
+
+  bool same = true;
+
+  for (int i = 0; i < res.size(); i++){
+    // Was marking -20.3172 as not equal to -20.3172 so adding a tolerance threshold
+    double abs(hadamard[i] - res[i]);
+    if (abs > 0.0001){
+      same = false;
+      cout << "\n";
+      cout << abs << "\n";
+      cout << hadamard[i] << ", ";
+      cout << res[i];
+      break;
+    }
+  }
+
+  cout << "\n\nProblem 6 (with residual tolerance of 0.0001): ";
+  cout << same;
+
+  cout << "\n\t\tNote that 1 is true annd 0 is false";
+
+  delete[] arr1;
+  delete[] arr2;
+  file.close();
+  file2.close();
+  file3.close();
+  file4.close();
+
+  return 0;
+}
